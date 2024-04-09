377d640cf4f3eb60269801341c23fc28bd94412d
Vishwesh Shukla <vishweshshukla20@gmail.com>
2024-04-09 08:44:32 +0530

    added bottom sheet for polls detail screen

---
+++ lib/screens/polls_screen/poll_details_screen.dart
@@ -0,0 +1,132 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:polmitra_admin/models/poll.dart';
+import 'package:polmitra_admin/utils/text_builder.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PollDetailsScreen extends StatefulWidget {
+  final Poll poll;
+  final String uploadedBy;
+  const PollDetailsScreen({required this.poll, required this.uploadedBy, super.key});
+
+  @override
+  State<PollDetailsScreen> createState() => _PollDetailsScreenState();
+}
+
+class _PollDetailsScreenState extends State<PollDetailsScreen> {
+  late final Poll _poll;
+  late final String _uploadedBy;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _poll = widget.poll;
+      _uploadedBy = widget.uploadedBy;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var totalResponses = _poll.responses.values.reduce((sum, element) => sum + element);
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                    Text(
+                      'Event Details',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      const TextSpan(
+                          text: "Active: ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: _poll.isActive ? "Yes" : "No",
+                          style: TextStyle(color: _poll.isActive ? Colors.green : Colors.red)),
+                    ],
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      const TextSpan(
+                          text: "Uploaded By: ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: _uploadedBy,
+                          style: const TextStyle(color: Colors.blue),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () async {
+                              final scaffold = ScaffoldMessenger.of(context);
+                              final mailtourl = 'mailto:$_uploadedBy';
+                              final uri = Uri.parse(mailtourl);
+                              if (await canLaunchUrl(uri)) {
+                                launchUrl(uri);
+                              } else {
+                                scaffold.showSnackBar(const SnackBar(content: Text("Could not launch email client")));
+                              }
+                            }),
+                    ],
+                  ),
+                ),
+
+                /// totalResponses is the sum of all responses for the poll.
+                TextBuilder.getText(
+                  text: "Total Responses: $totalResponses",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
+                const SizedBox(height: 10),
+
+                /// Display the options and their respective percentages.
+                Column(
+                    children: _poll.options.map(
+                          (option) {
+                        final voteCount =
+                            _poll.responses[option.toLowerCase()] ?? 0; // Ensure option string matches response keys exactly.
+                        final double percentage = totalResponses > 0 ? (voteCount / totalResponses) : 0.0;
+
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("$option: ${(percentage * 100).toStringAsFixed(2)}%"),
+                            SizedBox(
+                              height: 10,
+                              child: LinearProgressIndicator(
+                                value: percentage,
+                                backgroundColor: Colors.grey[300],
+                                valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
+                              ),
+                            ),
+                            const SizedBox(height: 10), // Spacing between bars
+                          ],
+                        );
+                      },
+                    ).toList())
+              ]),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/polls_screen/polls_screen.dart
+++ lib/screens/polls_screen/polls_screen.dart
@@ -1,13 +1,12 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polmitra_admin/bloc/polls/polls_bloc.dart';
 import 'package:polmitra_admin/bloc/polls/polls_event.dart';
 import 'package:polmitra_admin/bloc/polls/polls_state.dart';
 import 'package:polmitra_admin/models/poll.dart';
+import 'package:polmitra_admin/screens/polls_screen/poll_details_screen.dart';
 import 'package:polmitra_admin/utils/color_provider.dart';
 import 'package:polmitra_admin/utils/text_builder.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class PollsScreen extends StatefulWidget {
   const PollsScreen({super.key});
@@ -17,6 +16,8 @@ class PollsScreen extends StatefulWidget {
 }
 
 class _PollsScreenState extends State<PollsScreen> {
+  PersistentBottomSheetController? _pollBottomSheetController;
+
   @override
   void initState() {
     super.initState();
@@ -78,118 +79,33 @@ class _PollsScreenState extends State<PollsScreen> {
     final uploadedBy = poll.neta?.email ?? poll.netaId;
 
     return ListTile(
-        title: TextBuilder.getText(text: poll.question, fontWeight: FontWeight.bold, fontSize: 16),
-        subtitle: TextBuilder.getText(text: uploadedBy, fontWeight: FontWeight.normal, fontSize: 12),
-        leading: TextBuilder.getText(text: "${index + 1}", fontWeight: FontWeight.bold, fontSize: 16),
-        trailing: Switch(
-          activeColor: Colors.blue,
-          trackColor: MaterialStateColor.resolveWith((states) => Colors.grey),
-          value: poll.isActive,
-          onChanged: (value) {
-            BlocProvider.of<PollBloc>(context).add(UpdatePollActiveStatus(poll.id, value));
-          },
-        ),
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return _showAdaptiveDialog(poll, uploadedBy);
-            },
-          );
-        });
+      title: TextBuilder.getText(text: poll.question, fontWeight: FontWeight.bold, fontSize: 16),
+      subtitle: TextBuilder.getText(text: uploadedBy, fontWeight: FontWeight.normal, fontSize: 12),
+      leading: TextBuilder.getText(text: "${index + 1}", fontWeight: FontWeight.bold, fontSize: 16),
+      trailing: Switch(
+        activeColor: Colors.blue,
+        trackColor: MaterialStateColor.resolveWith((states) => Colors.grey),
+        value: poll.isActive,
+        onChanged: (value) {
+          BlocProvider.of<PollBloc>(context).add(UpdatePollActiveStatus(poll.id, value));
+        },
+      ),
+      onTap: () => _showPollDetailsBottomSheet(poll, uploadedBy),
+    );
   }
 
-  AlertDialog _showAdaptiveDialog(Poll poll, String uploadedBy) {
-    var totalResponses = poll.responses.values.reduce((sum, element) => sum + element);
-
-    return AlertDialog(
-      title: TextBuilder.getText(text: poll.question, fontWeight: FontWeight.bold, fontSize: 16),
-      content: SingleChildScrollView(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              RichText(
-                text: TextSpan(
-                  children: [
-                    const TextSpan(
-                        text: "Active: ", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                    TextSpan(
-                        text: poll.isActive ? "Yes" : "No",
-                        style: TextStyle(color: poll.isActive ? Colors.green : Colors.red)),
-                  ],
-                ),
-              ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    const TextSpan(
-                        text: "Uploaded By: ",
-                        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                    TextSpan(
-                        text: uploadedBy,
-                        style: const TextStyle(color: Colors.blue),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () async {
-                            final scaffold = ScaffoldMessenger.of(context);
-                            final mailtourl = 'mailto:$uploadedBy';
-                            final uri = Uri.parse(mailtourl);
-                            if (await canLaunchUrl(uri)) {
-                              launchUrl(uri);
-                            } else {
-                              scaffold.showSnackBar(
-                                  const SnackBar(content: Text("Could not launch email client")));
-                            }
-                          }),
-                  ],
-                ),
-              ),
-
-              /// totalResponses is the sum of all responses for the poll.
-              TextBuilder.getText(
-                text: "Total Responses: $totalResponses",
-                fontWeight: FontWeight.bold,
-                fontSize: 12,
-              ),
-              const SizedBox(height: 10),
-
-              /// Display the options and their respective percentages.
-              Column(
-                  children: poll.options.map(
-                        (option) {
-                      final voteCount = poll.responses[option.toLowerCase()] ??
-                          0; // Ensure option string matches response keys exactly.
-                      final double percentage = totalResponses > 0 ? (voteCount / totalResponses) : 0.0;
-
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("$option: ${(percentage * 100).toStringAsFixed(2)}%"),
-                          SizedBox(
-                            height: 10,
-                            child: LinearProgressIndicator(
-                              value: percentage,
-                              backgroundColor: Colors.grey[300],
-                              valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-                            ),
-                          ),
-                          const SizedBox(height: 10), // Spacing between bars
-                        ],
-                      );
-                    },
-                  ).toList())
-            ]),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: TextBuilder.getText(text: "Close", fontWeight: FontWeight.bold, fontSize: 16),
-        ),
-      ],
+  void _showPollDetailsBottomSheet(Poll poll, String uploadedBy) {
+    _pollBottomSheetController = showBottomSheet(
+      context: context,
+      builder: (context) {
+        return PollDetailsScreen(poll: poll, uploadedBy: uploadedBy);
+      },
     );
+  }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _pollBottomSheetController?.close();
   }
 }
