e40a3b6f03ffd411ebb7a2c028828e681d740943
Vishwesh Shukla <vishweshshukla20@gmail.com>
2024-04-09 10:05:38 +0530

    added filtering for events

--- lib/screens/events_screen/events_screen.dart
+++ lib/screens/events_screen/events_screen.dart
@@ -1,11 +1,15 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polmitra_admin/bloc/polmitra_event/pevent_bloc.dart';
 import 'package:polmitra_admin/bloc/polmitra_event/pevent_event.dart';
 import 'package:polmitra_admin/bloc/polmitra_event/pevent_state.dart';
 import 'package:polmitra_admin/models/event.dart';
+import 'package:polmitra_admin/models/indian_state.dart';
 import 'package:polmitra_admin/screens/events_screen/event_details_screen.dart';
+import 'package:polmitra_admin/utils/city_state_provider.dart';
 import 'package:polmitra_admin/utils/color_provider.dart';
 import 'package:polmitra_admin/utils/text_builder.dart';
 
@@ -19,11 +23,20 @@ class EventsScreen extends StatefulWidget {
 class _EventsScreenState extends State<EventsScreen> {
   PersistentBottomSheetController? _eventBottomSheetController;
 
+  String _searchQuery = '';
+  bool _isActiveFilter = true;
+  String _selectedCity = 'All';
+  String _selectedState = 'All';
+  List<String> _cities = ['All']; // This will be populated based on the selected state
+  List<String> _states = ['All']; // Populate this with the list of states
+
   @override
   void initState() {
     super.initState();
     _loadEvents();
-    _fetchCurrentUser();
+
+    CityStateProvider(); // triggers fetching of states and cities
+    _fetchStatesAndCities();
   }
 
   Future<void> _loadEvents() async {
@@ -32,7 +45,126 @@ class _EventsScreenState extends State<EventsScreen> {
     bloc.add(LoadEvents());
   }
 
-  void _fetchCurrentUser() async {}
+  void _fetchStatesAndCities() async {
+    var states = CityStateProvider().states;
+    // Assuming 'states' is now a list of IndianState objects
+    setState(() {
+      _states = ['All'] + states.map((state) => state.statename).toList();
+      _cities = ['All']; // Default cities list
+    });
+  }
+
+  List<Event> _filterEvents(List<Event> eventsEntry) {
+    return eventsEntry.where((event) {
+      // Check if the event matches the search query, if any
+      bool matchesSearchQuery = _searchQuery.isEmpty || event.eventName.toLowerCase().contains(_searchQuery);
+
+      // Check if the event matches the active status
+      bool matchesActiveStatus = !_isActiveFilter || event.isActive;
+
+      // Check if the event matches the selected state, if any
+      bool matchesState = _selectedState == 'All' || event.state.statename == _selectedState;
+
+      // Check if the event matches the selected city, if any
+      bool matchesCity = _selectedCity == 'All' || event.city.cityname == _selectedCity;
+
+      return matchesSearchQuery && matchesActiveStatus && matchesState && matchesCity;
+    }).toList();
+  }
+
+  Widget _buildFilterBar() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          /// name search field
+          Container(
+            width: 200,
+            padding: const EdgeInsets.all(8),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value.toLowerCase();
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'Search by name',
+                suffixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 200,
+                padding: const EdgeInsets.all(8),
+                child: DropdownButton<String>(
+                  value: _selectedState,
+                  items: _states.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: SizedBox(width: 150, child: TextBuilder.getText(text: value, fontSize: 14)),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      // _selectedState = newValue!;
+                      // _selectedCity = 'All'; // Reset city selection when state changes
+                      // // Update city list based on selected state
+
+                      _selectedState = newValue!;
+                      if (newValue != 'All') {
+                        var selectedStateObj = CityStateProvider().states.firstWhere(
+                              (state) => state.statename == newValue,
+                              orElse: () => IndianState(stateid: 0, statename: '', cities: []),
+                            );
+                        _cities = ['All'] + selectedStateObj.cities.map((city) => city.cityname).toList();
+                      } else {
+                        _cities = ['All'];
+                      }
+                      _selectedCity = 'All'; // Reset city selection when state changes
+                    });
+                  },
+                ),
+              ),
+              Container(
+                width: 200,
+                padding: const EdgeInsets.all(8),
+                child: DropdownButton<String>(
+                  value: _selectedCity,
+                  items: _cities.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: SizedBox(width: 150, child: TextBuilder.getText(text: value, fontSize: 14)),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedCity = newValue!;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                width: 70,
+                padding: const EdgeInsets.all(8),
+                child: Switch(
+                  activeColor: Colors.blue,
+                  trackColor: MaterialStateColor.resolveWith((states) => Colors.grey),
+                  value: _isActiveFilter,
+                  onChanged: (value) {
+                    setState(() {
+                      _isActiveFilter = value;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,32 +175,14 @@ class _EventsScreenState extends State<EventsScreen> {
             child: CircularProgressIndicator(),
           );
         } else if (state is EventsLoaded) {
-          return Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: ListView.builder(
-              itemCount: state.events.length,
-              itemBuilder: (context, index) {
-                final event = state.events[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: ColorProvider.normalWhite,
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: ColorProvider.darkGreyColor,
-                          offset: const Offset(0.0, 1.5), //(x,y)
-                          blurRadius: 8.0,
-                        ),
-                      ],
-                    ),
-                    child: _buildEventCard(event),
-                  ),
-                );
-              },
-            ),
+          return Column(
+            children: [
+              _buildFilterBar(),
+              Expanded(child: _buildEventList(_filterEvents(state.events))),
+            ],
           );
+
+          return _buildEventList(state.events);
         } else if (state is EventError) {
           return Center(
             child: Text(state.message),
@@ -82,47 +196,72 @@ class _EventsScreenState extends State<EventsScreen> {
     );
   }
 
+  Widget _buildEventList(List<Event> eventsEntry) {
+    return ListView.builder(
+      itemCount: eventsEntry.length,
+      itemBuilder: (context, index) {
+        final event = eventsEntry[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+          child: Container(
+            decoration: BoxDecoration(
+              color: ColorProvider.normalWhite,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: ColorProvider.darkGreyColor,
+                  offset: const Offset(0.0, 1.5), //(x,y)
+                  blurRadius: 8.0,
+                ),
+              ],
+            ),
+            child: _buildEventCard(event),
+          ),
+        );
+      },
+    );
+  }
+
   /// Event card widget
   Widget _buildEventCard(Event event) {
     return ListTile(
-      title: TextBuilder.getText(
-          text: event.eventName, fontWeight: FontWeight.bold, fontSize: 14, overflow: TextOverflow.ellipsis),
-      subtitle: TextBuilder.getText(text: event.description, overflow: TextOverflow.ellipsis, fontSize: 12),
+        title: TextBuilder.getText(
+            text: event.eventName, fontWeight: FontWeight.bold, fontSize: 14, overflow: TextOverflow.ellipsis),
+        subtitle: TextBuilder.getText(text: event.description, overflow: TextOverflow.ellipsis, fontSize: 12),
 
-      /// leading image
-      leading: SizedBox(
-        width: 80,
-        child: CachedNetworkImage(
-          imageBuilder: (context, imageProvider) => Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              image: DecorationImage(
-                image: imageProvider,
-                fit: BoxFit.cover,
+        /// leading image
+        leading: SizedBox(
+          width: 80,
+          child: CachedNetworkImage(
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
+            imageUrl: event.images.isNotEmpty ? event.images.first : 'https://via.placeholder.com/150',
+            errorWidget: (context, url, error) => const Icon(Icons.error),
           ),
-          imageUrl: event.images.isNotEmpty ? event.images.first : 'https://via.placeholder.com/150',
-          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
-      ),
 
-      /// isActive switch
-      trailing: Transform.scale(
-        scale: 0.8,
-        child: Switch(
-          activeColor: Colors.blue,
-          trackColor: MaterialStateColor.resolveWith((states) => Colors.grey),
-          value: event.isActive,
-          onChanged: (bool eventActiveStatus) {
-            BlocProvider.of<EventBloc>(context).add(UpdateEventActiveStatus(event.id, eventActiveStatus));
-          },
+        /// isActive switch
+        trailing: Transform.scale(
+          scale: 0.8,
+          child: Switch(
+            activeColor: Colors.blue,
+            trackColor: MaterialStateColor.resolveWith((states) => Colors.grey),
+            value: event.isActive,
+            onChanged: (bool eventActiveStatus) {
+              BlocProvider.of<EventBloc>(context).add(UpdateEventActiveStatus(event.id, eventActiveStatus));
+            },
+          ),
         ),
-      ),
 
-      /// on tap
-      onTap: () => _showEventBottomSheet(event)
-    );
+        /// on tap
+        onTap: () => _showEventBottomSheet(event));
   }
 
   void _showEventBottomSheet(Event event) {
