8ca14bfd514fa2a6bad7a9a9868007e72a7a0232
Vishwesh Shukla <vishweshshukla20@gmail.com>
2024-04-09 09:38:58 +0530

    added filter to search by name for users

--- lib/screens/users_screen/users_screen.dart
+++ lib/screens/users_screen/users_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:polmitra_admin/bloc/users/user_bloc.dart';
@@ -9,7 +8,6 @@ import 'package:polmitra_admin/enums/user_enums.dart';
 import 'package:polmitra_admin/models/user.dart';
 import 'package:polmitra_admin/screens/users_screen/user_details_screen.dart';
 import 'package:polmitra_admin/utils/text_builder.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 // Stateful widget for the Users Screen
 class UsersScreen extends StatefulWidget {
@@ -23,13 +21,17 @@ class _UsersScreenState extends State<UsersScreen> {
   // Initial filter options
   FilterRoleOption _filterRoleOption = FilterRoleOption.all;
   FilterActiveOption _filterActive = FilterActiveOption.all;
+  final TextEditingController _searchController = TextEditingController();
 
   PersistentBottomSheetController? _userDetailsBottomSheetController;
+  List<PolmitraUser> users = [];
+  List<PolmitraUser> filteredUsers = [];
 
   @override
   void initState() {
     super.initState();
     _loadUsers(); // Load users at startup
+    _searchController.addListener(_filterUsers);
   }
 
   // Method to load users via the UserBloc
@@ -37,9 +39,19 @@ class _UsersScreenState extends State<UsersScreen> {
     BlocProvider.of<UserBloc>(context).add(LoadUsers());
   }
 
+  void _filterUsers() {
+    setState(() {
+      filteredUsers = _applyFilters();
+    });
+  }
+
   // Method to filter the list of users based on role and active status
-  List<PolmitraUser> _applyFilters(List<PolmitraUser> users) {
+  List<PolmitraUser> _applyFilters() {
+    String searchQuery = _searchController.text.trim().toLowerCase();
     return users.where((user) {
+      bool searchMatches =
+          user.name.toLowerCase().contains(searchQuery) || user.email.toLowerCase().contains(searchQuery);
+
       bool roleMatches;
       switch (_filterRoleOption) {
         case FilterRoleOption.superadmin:
@@ -69,66 +81,105 @@ class _UsersScreenState extends State<UsersScreen> {
           activeMatches = true;
       }
 
-      return roleMatches && activeMatches;
+      return roleMatches && activeMatches && searchMatches;
     }).toList();
+  }
+
+  Widget _searchField() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: TextField(
+        controller: _searchController,
+        decoration: const InputDecoration(
+          labelText: 'Search by name or email',
+          labelStyle: TextStyle(fontSize: 14),
+          suffixIcon: Icon(Icons.search),
+          border: UnderlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Widget _filterWidgetsRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        DropdownButton<FilterActiveOption>(
+          value: _filterActive,
+          onChanged: (value) {
+            setState(() {
+              _filterActive = value ?? FilterActiveOption.all;
+            });
+          },
+          items: FilterActiveOption.values.map((option) {
+            String text = option == FilterActiveOption.all
+                ? 'All Users'
+                : option == FilterActiveOption.active
+                    ? 'Active Users'
+                    : 'Inactive Users';
+            return DropdownMenuItem(
+              value: option,
+              child: Text(text),
+            );
+          }).toList(),
+        ),
+        const SizedBox(width: 20),
+        DropdownButton<FilterRoleOption>(
+          value: _filterRoleOption,
+          onChanged: (value) {
+            setState(() {
+              _filterRoleOption = value ?? FilterRoleOption.all;
+            });
+          },
+          items: FilterRoleOption.values.map((option) {
+            String text = option.toString().split('.').last.capitalize();
+            return DropdownMenuItem(
+              value: option,
+              child: Text(text),
+            );
+          }).toList(),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<UserBloc, UserState>(
+      body: BlocConsumer<UserBloc, UserState>(
+        buildWhen: (previous, current) {
+          return current is UserLoading || current is UsersLoaded || current is UserError;
+        },
+        listener: (context, state) {
+          if (state is UserError) {
+            // Display error message
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Error loading users: ${state.message}")));
+          } else if (state is UsersLoaded) {
+            // Display success message
+            setState(() {
+              users = state.users;
+              filteredUsers = _applyFilters();
+            });
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Users loaded successfully.")));
+          }
+        },
         builder: (context, state) {
           if (state is UserLoading) {
-            // Display loading indicator while users are being fetched
             return const Center(child: CircularProgressIndicator());
           } else if (state is UsersLoaded) {
-            // Filter users based on selected criteria
-            var filteredUsers = _applyFilters(state.users);
+            _applyFilters();
             return Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
-                  // Filter dropdown menus
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      DropdownButton<FilterActiveOption>(
-                        value: _filterActive,
-                        onChanged: (value) {
-                          setState(() {
-                            _filterActive = value ?? FilterActiveOption.all;
-                          });
-                        },
-                        items: FilterActiveOption.values.map((option) {
-                          String text = option == FilterActiveOption.all
-                              ? 'All Users'
-                              : option == FilterActiveOption.active
-                                  ? 'Active Users'
-                                  : 'Inactive Users';
-                          return DropdownMenuItem(
-                            value: option,
-                            child: Text(text),
-                          );
-                        }).toList(),
-                      ),
-                      const SizedBox(width: 20),
-                      DropdownButton<FilterRoleOption>(
-                        value: _filterRoleOption,
-                        onChanged: (value) {
-                          setState(() {
-                            _filterRoleOption = value ?? FilterRoleOption.all;
-                          });
-                        },
-                        items: FilterRoleOption.values.map((option) {
-                          String text = option.toString().split('.').last.capitalize();
-                          return DropdownMenuItem(
-                            value: option,
-                            child: Text(text),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
+                  /// search field
+                  _searchField(),
+
+                  /// Display the filter options
+                  _filterWidgetsRow(),
+
+                  /// Display the list of users
                   Expanded(
                     child: ListView.builder(
                       itemCount: filteredUsers.length,
